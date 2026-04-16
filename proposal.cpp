7e3840afc71df1633ff817b614f207970ecb7e70
Akashdeep <akashdeep.thind.singh@gmail.com>
2026-04-15 21:22:24 -0700

    third commit

--- proposal.cpp
+++ proposal.cpp
@@ -7,6 +7,7 @@
 #include <array>
 #include <list>
 #include <fstream>
+#include <string>
 using namespace std;
 
 // CONSTANTS
@@ -22,32 +23,43 @@ int main ()
     //PSEDUOCODE:
     //Creat map<string, array<list<string>, 3>>
     map<string, array<list<string>, NUM_LISTS>> hospital;
-
+    
+    //Open external file
     ifstream fin("patients.txt");
 
+    // Check file error
     if (!fin)
     {
         cout << "Error opening file.\n";
         return 1;
     }
 
+    // Reads file and than populates map
+    //Each line: department patientName
     string dept, name;
     while (fin >> dept >> name)
     { 
+        // Insert patient into the waiting list (index 0)
         hospital[dept][0].push_back(name);
 }
 
 fin.close();
 
+//Displays initial state
 cout << "INITIAL STATE:\n";
 display(hospital);
 
+// Run simulation
+simulate(hospital);
+
+//Display final state
 cout << "\nFINAL STATE:\n";
     display(hospital);
 
     return 0;
 }
 
+//Function: display
 void display(const map<string, array<list<string>, NUM_LISTS>>& hospital)
 {
     // Loop through each department
@@ -69,5 +81,53 @@ void display(const map<string, array<list<string>, NUM_LISTS>>& hospital)
             cout << p << " ";
 
         cout << endl;
+    }
+}
+
+
+void simulate(map<string, array<list<string>, NUM_LISTS>>& hospital)
+{
+    
+    for (int t = 1; t <= TIME_STEPS; t++)
+    {
+        cout << "\n--- Time Step " << t << " ---\n";
+
+        for (auto& pair : hospital)
+        {
+            string dept = pair.first;
+
+
+           
+
+            if (!pair.second[0].empty())
+            {
+                string patient = pair.second[0].front();
+                pair.second[0].pop_front();
+                pair.second[1].push_back(patient);
+
+                cout << patient << " moved to treatment in " << dept << endl;
+            }
+
+            // If treatment list is not empty than move one patient to discharged
+
+            if (!pair.second[1].empty())
+            {
+                string patient = pair.second[1].front();
+                pair.second[1].pop_front();
+                pair.second[2].push_back(patient);
+
+                cout << patient << " discharged from " << dept << endl;
+            }
+
+            // Optional: random new patient arrival, (mockup version just adds a dummy name occasionally)
+
+            if (t % 5 == 0)
+            {
+                string newPatient = "NewPatient" + to_string(t);
+                pair.second[0].push_back(newPatient);
+
+                cout << newPatient << " arrived at " << dept << endl;
+            }
+        }
     }
 }
